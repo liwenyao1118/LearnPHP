7c78b1617c3d822d90ab9455b6af3c938c088c48
liwenyao <975139902@qq.com>
2020-04-01 00:27:21 +0800

    语法练习

--- Base/1_10.php
+++ Base/1_10.php
@@ -6,6 +6,11 @@
     <title>Document</title>
 </head>
 <body>
-    <h1></h1>
+    <h1>PHP有7种数据类型，分别是字符串类型、整型、浮点型、布尔型、数组类型、对象类型、null</h1>
+    <h1>数字类型</h1>
+
+    <?php
+        
+    ?>
 </body>
 </html>
