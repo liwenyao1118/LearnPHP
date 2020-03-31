c498bdbf7be9855d92acbecc6fe13add2a70070d
liwenyao <975139902@qq.com>
2020-04-01 00:53:46 +0800

    '数据类型-布尔类型'

---
+++ Base/1_12.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>PHP有7种数据类型，分别是字符串类型、整型、浮点型、布尔型、数组类型、对象类型、null</h1>
+    <h1>布尔类型</h1>
+
+    <?php
+        $a = true;
+        $b = false;
+        var_dump($a);
+        echo '<br>';
+        var_dump($b);
+        echo "<br>${a},${b}";
+    ?>
+</body>
+</html>
