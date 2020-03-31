5435a1aa458858515fbf10459beb42957715ebf0
liwenyao <975139902@qq.com>
2020-04-01 00:40:50 +0800

    数据类型-整型

--- Base/1_10.php
+++ Base/1_10.php
@@ -10,7 +10,27 @@
     <h1>数字类型</h1>
 
     <?php
+        $a = 1;
+        var_dump($a);       //输出数据类型及变量的值
+
+        echo '<br>';
+
+        $b = 2.0;
+        var_dump($b);
+
+
+        echo '<br>';
+
+        $c = -3;
+        var_dump($c);
+
+        echo '<br>';
         
+        $d = 017;       //八进制
+        $e = 0x1C;       //十六进制
+        var_dump($d);
+        echo '<br>';
+        var_dump($e);
     ?>
 </body>
 </html>
